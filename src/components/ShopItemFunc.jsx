96f7fb8bc6e52a6e3c69a0003dad475a272f82e6
Cen-2-rion <atrohov1982@gmail.com>
2024-11-14 02:38:09 +0300

    modified ShopItemFunc.jsx

--- src/components/ShopItemFunc.jsx
+++ src/components/ShopItemFunc.jsx
@@ -1,21 +1,20 @@
 import './ShopItemFunc.css';
 import PropTypes from "prop-types";
 
-const ShopItemFunc = (props) => {
-    const {brand, title, description, descriptionFull, price, currency} = props.item;
+const ShopItemFunc = ({item}) => {
 
     return (
         <div className="main-content">
-            <h2>{brand}</h2>
-            <h1>{title}</h1>
-            <h3>{description}</h3>
-            <div className="description">{descriptionFull}</div>
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">{item.descriptionFull}</div>
             <div className="highlight-window mobile">
                 <div className="highlight-overlay"></div>
             </div>
             <div className="divider"></div>
             <div className="purchase-info">
-                <div className="price">{currency}{price.toFixed(2)}</div>
+                <div className="price">{item.currency}{item.price.toFixed(2)}</div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
